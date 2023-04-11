a8b9a0d7055af93c127ed724e84a44770acd6702
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-04-11 23:17:10 +0530

    Wrote the main function code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.example.game.Game;
+import org.example.player.Player;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Game game = new Game();
+
+        System.out.println("Enter no. of players");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Player " + (i + 1) + "'s name");
+            String playerName = sc.next();
+            game.addPlayer(new Player(playerName));
+        }
+
+        game.start();
+
+        while (game.isGameActive()) {
+            game.playTurn();
+        }
+
+        System.out.println(game.getGameResult());
     }
 }
