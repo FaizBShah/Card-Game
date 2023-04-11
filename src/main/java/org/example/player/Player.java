7dbe987ed13eac4c34e2c59189242b7b009d15b4
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-04-11 20:29:10 +0530

    Player entity created

---
+++ src/main/java/org/example/player/Player.java
@@ -0,0 +1,38 @@
+package org.example.player;
+
+import org.example.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String name;
+    private final List<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void addCardsToHand(List<Card> cards) {
+        hand.addAll(cards);
+    }
+
+    public void removeCardFromHand(Card card) {
+        if (!hand.remove(card)) {
+            throw new IllegalStateException("The card does not belong to " + name + "'s hand");
+        }
+    }
+}
